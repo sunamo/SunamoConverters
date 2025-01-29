7d267eae3960216e92eb09e795c65eb3afebc657
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 20:59:39 +0100

    25.1.29.1 Deleted private, protected, internal unused methods

--- _sunamo/SunamoShared/HttpUtility.cs
+++ _sunamo/SunamoShared/HttpUtility.cs
@@ -10,16 +10,4 @@ internal class HttpUtility //: SunamoExceptions.InSunamoIsDerivedFrom.HttpUtilit
     {
         return HttpUtility.ParseQueryString(responseContent);
     }
-    internal static string HtmlEncodeWithCompatibility(string html, bool backwardCompatibility = true)
-    {
-        if (html == null)
-        {
-            throw new Exception("html");
-        }
-        // replace & by &amp; but only once!
-        Regex rx = backwardCompatibility
-            ? new Regex("&(?!(amp;)|(lt;)|(gt;)|(quot;))", RegexOptions.IgnoreCase)
-            : new Regex("&(?!(amp;)|(lt;)|(gt;)|(quot;)|(nbsp;)|(reg;))", RegexOptions.IgnoreCase);
-        return rx.Replace(html, "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("\"", "&quot;");
-    }
 }
